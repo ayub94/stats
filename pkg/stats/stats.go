f9e0b591d65ffc6d21f4f54acfc28f41f72f9d0d
Ayub <ayubjon94.an@gmail.com>
2020-09-26 15:52:45 +0500

    добавлена новые условии

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,7 +1,7 @@
 package stats
 
 import (
-	"github.com/ayub94/bank/pkg/types"
+	"github.com/ayub94/bank/v2/pkg/types"
 
 )
 
@@ -10,19 +10,23 @@ func Avg(payments []types.Payment) types.Money  {
 	averagesum := types.Money(0)
     sum := types.Money(0)
 	for _, payment := range payments {
-	sum += payment.Amount
-	averagesum = sum / (types.Money(len(payments)))
+		if payment.Status == "Ok" || payment.Status == "INPROGRESS"{
+	            sum += payment.Amount
+	            averagesum = sum / (types.Money(len(payments)))
+		}
 	
 	}
 
 	return averagesum
 }
-//const countcategory = "Cafe"
+
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money  {
 	summincategory := types.Money(0)
 	for _, payment := range payments{
 		if payment.Category== category {
+			if payment.Status == "Ok" || payment.Status == "INPROGRESS" {
 			summincategory += payment.Amount
+			}
 		}
 		
 	}
