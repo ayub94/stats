044e0a1e73a95bdac944ef2553c33494a9d74b08
Ayub <ayubjon94.an@gmail.com>
2020-09-27 18:26:52 +0500

    new commit 3

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -9,16 +9,15 @@ func Avg(payments []types.Payment) types.Money  {
 	
 	averagesum := types.Money(0)
 	sum := types.Money(0)
-	l := 0
+	var l int 
 	for _, payment := range payments {
 		if payment.Status == types.StatusOk || payment.Status == types.StatusInProgress {
-
 			    l+=1
 	            sum += payment.Amount
 		}
-		averagesum = sum / (types.Money(l))
 	}
-
+	
+    averagesum = sum / (types.Money(l))
 	return averagesum
 }
 
